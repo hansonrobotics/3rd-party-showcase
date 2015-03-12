9cc708e1e1456c7307a481d583cc3450e4e5a017
Gabrielius Mickevicius <gabrielius.m@gmail.com>
2015-03-12 11:49:21 +0200

    Add VeriLookFace

---
+++ VeriLookFace/README.md
@@ -0,0 +1,15 @@
+# VeriLook Face Identification Algorithm #
+
+Neurotechnology's VeriLook face identification demo can do alive face detection, simultaneous multiple face recognition and fast face matching in 1-to-1 and 1-to-many modes.
+
+[More info here.](http://neurotechnology.com/verilook.html)
+
+[Demo downloaded from here](http://neurotechnology.com/download.html#demo).
+
+## Run ##
+
+```bash
+./run.sh
+```
+
+User Interface is self explanatory.
---
+++ VeriLookFace/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Would've done a docker image instead, but for some reason the demo can't
+# find the webcam from a docker container, even with /dev/video0 mounted.
+
+# Exit if any of the commands fail
+set -e
+
+DIRNAME=Neurotec_Biometric_5_1_Algorithm_Demo_Linux_x86_64
+
+# Download demo if it's not already there
+if test ! -d $DIRNAME; then
+  wget -O tmp.zip http://download.neurotechnology.com/$DIRNAME\_2015-03-02.zip
+  unzip tmp.zip
+  rm tmp.zip
+fi
+
+# Run
+./$DIRNAME/FacesAlgorithmDemo
